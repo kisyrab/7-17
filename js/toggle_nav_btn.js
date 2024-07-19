c5d89fc251311e5d81d638389eddcadb014039de
kisyrab <906ksy@naver.com>
2024-07-19 12:11:54 +0900

    first commit

--- js/toggle_nav_btn.js
+++ js/toggle_nav_btn.js
@@ -3,4 +3,16 @@ $(function(){
         $(".gnb").stop().slideToggle("fast");
     })// menu_toggle_btn
 
+    $(".search_select_box span").hover(function(){
+        // $(this).next().stop().slideDown();
+        $(".search_select_list").stop().slideDown();
+    },function(){
+        // $(this).next().stop().slideUp();
+        $(".search_select_list").stop().slideUp();
+
+    })
+
+
+
+
 })///j
